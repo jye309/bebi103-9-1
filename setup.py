d7f69a1254978803ab6c4f04b450374ae745e851
jye309 <53058496+jye309@users.noreply.github.com>
2019-12-10 15:21:03 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+def read(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+
+long_description = read('README.md')
+
+setup(
+    name='beb103_9_2',
+    version='0.0.1',
+    url='https://github.com/Lioscro/bebi103-9-2',
+    description='Pip-installable package with functions used in hw9.2',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=read('requirements.txt').strip().split('\n'),
+)
