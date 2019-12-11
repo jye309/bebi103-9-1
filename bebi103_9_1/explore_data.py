34428653ba8e73f26ba1f8dcb6246200e27bb52f
jye309 <53058496+jye309@users.noreply.github.com>
2019-12-11 00:39:48 -0800

    Update explore_data.py

--- bebi103_9_1/explore_data.py
+++ bebi103_9_1/explore_data.py
@@ -26,28 +26,30 @@ def display_ecdf(df, cats, val):
     bokeh.io.show(p)
     
 def display_strip_box(df, kdmis, vdims):
-    strip = hv.Scatter(
-        data=df,
-        kdims=kdmis,
-        vdims=vdims,
-    ).opts(
-        color='concentration',
-        jitter=0.5,
-        xlabel='concentration',
-        alpha=0.5, 
+    # Make a box plot
+    p = bokeh_catplot.box(
+        data=df.dropna(),
+        cats='concentration',
+        val='time to catastrophe (s)',
+        horizontal=False,
+        box_kwargs=dict(fill_color='gray', fill_alpha=0.5),
+        display_points=False, 
+        width=600,
         height=500,
-        width=500
+        whisker_caps=True,
     )
 
-    box = hv.BoxWhisker(
-        data=df,
-        kdims=kdmis,
-        vdims=vdims,
-    ).opts(
-        box_fill_color='lightgray',
-        outlier_alpha=0,
-        height=500,
-        width=500
+    # Overlay a jitter plot
+    p = bokeh_catplot.strip(
+        data=df.dropna(),
+        cats='concentration',
+        val='time to catastrophe (s)',
+        p=p,
+        horizontal=False,
+        jitter=True,
+        marker_kwargs=dict(alpha=0.1),
+        width=600,
+        height=500
     )
-
-    return box * strip
+    
+    bokeh.io.show(p)
